c3871bd48355309f0f7ea89b7b8a85fd74c2b0cc
abhinav-tomar-251 <abhinavtomar251@gmail.com>
2025-01-13 10:12:25 +0530

    primitive datatype

---
+++ cpp/01_varconst/data_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int tealeaves = 50;
+    //for diffrent precision a we use diffrent datatypes such as
+    float waterTeamperature = 90.8;
+    double priceofTea = 288.9876;
+    char qualityofTea = 'C';
+    bool isTeaready = 0;
+     
+        cout<< priceofTea <<endl;
+
+    return 0;
+
+
+};
---
+++ cpp/01_varconst/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"starting with variables"<<endl;
+    return 0;
+
+}
+ 
