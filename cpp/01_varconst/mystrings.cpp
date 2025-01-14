8f4128e4eff009f4cba3b03c3721e0e3aaad2050
abhinav-tomar-251 <abhinavtomar251@gmail.com>
2025-01-14 13:29:16 +0530

    mystrings

--- cpp/01_varconst/mystrings.cpp
+++ cpp/01_varconst/mystrings.cpp
@@ -2,9 +2,10 @@
 #include <string>
 using namespace std;
 int main(){
-    string favoriteSweets = "Boondi Ladoo";
+    string favoriteSweets = "Boondi Ladoo \n";
     string description = "Sweet made up of boondi in \" ghee \" and is round shaped";
     cout << favoriteSweets << description << endl;
+    // here we have used escape sequences for changong lines and double quoting 
 
 
     return 0;
