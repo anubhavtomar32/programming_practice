92467d31c0600e2f59b0f4139c4b37158e642461
abhinav-tomar-251 <abhinavtomar251@gmail.com>
2025-01-14 12:35:44 +0530

    modifiers in datatypes

---
+++ cpp/01_varconst/modifier.cpp
@@ -0,0 +1,24 @@
+ #include <iostream>
+
+ using namespace std;
+
+ int main(){
+  
+  unsigned int numberofTeapackets = 100;
+
+  short readyTeapackets = 30;
+
+  long totalTeapackets = 100000; 
+
+  long long priceoftotalTeapackets = 3000000000;
+
+   cout << priceoftotalTeapackets << endl;
+   
+   cout << totalTeapackets << endl;
+
+   cout << readyTeapackets << endl;
+
+   cout << numberofTeapackets << endl;
+
+    return 0;
+ }
