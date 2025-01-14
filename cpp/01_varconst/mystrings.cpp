f176b1ed89069a39dc481b53d7bda7e439c60bf9
abhinav-tomar-251 <abhinavtomar251@gmail.com>
2025-01-14 13:24:55 +0530

    mystrings

---
+++ cpp/01_varconst/mystrings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string favoriteSweets = "Boondi Ladoo";
+    string description = "Sweet made up of boondi in \" ghee \" and is round shaped";
+    cout << favoriteSweets << description << endl;
+
+
+    return 0;
+
+}
